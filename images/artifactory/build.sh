dd9e977077fc650157d08dfc1c764e48145c1177
Simon Alman <haven@thehavennet.org.uk>
2019-02-04 14:49:12 +0000

    Fix paths to be precise

--- images/artifactory/build.sh
+++ images/artifactory/build.sh
@@ -6,7 +6,7 @@ configure_bob()
 
 configure_rootfs_build()
 {
-    emerge curl rsync layman
+    emerge net-misc/curl net-misc/rsync app-portage/layman
     configure_layman
     add_overlay haven-overlay https://github.com/thehaven/haven-overlay.git
     update_keywords '=dev-util/artifactory-pro-bin-6.2.0' '+~amd64'
