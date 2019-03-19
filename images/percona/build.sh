04ae3270c8efb45ae76c8f570c961b6706007f45
Simon Alman <haven@thehavennet.org.uk>
2019-03-19 22:57:42 +0000

    percona changes

--- images/percona/build.sh
+++ images/percona/build.sh
@@ -2,6 +2,7 @@ _packages="app-arch/pbzip2 net-misc/curl dev-db/percona-server"
 
 configure_rootfs_build()
 {
+    emerge --sync && emerge --oneshot portage
     unprovide_package net-misc/curl
     update_keywords 'dev-db/percona-server' '+~amd64'
     update_use 'dev-db/percona-server' '-client-libs +jemalloc -perl'
@@ -9,6 +10,7 @@ configure_rootfs_build()
 
 finish_rootfs_build()
 {
+    mkdir -p /var/run/mysqld/ && chown mysql:mysql /var/run/mysqld
     copy_gcc_libs
     uninstall_package net-misc/curl
 }
