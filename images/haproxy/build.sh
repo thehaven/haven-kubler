cb1cc817fe82af1d2bc180b491a01407ab998e9d
Simon Alman <haven@thehavennet.org.uk>
2019-06-30 13:37:19 +0100

    Updating openssl to 1.1.1 for haproxy

--- images/haproxy/build.sh
+++ images/haproxy/build.sh
@@ -1,10 +1,11 @@
-_packages="net-proxy/haproxy"
+_packages="net-proxy/haproxy dev-libs/openssl"
 
 configure_bob()
 {
-    #rm -Rf /var/sync/portage && emerge --sync && etc-update --automode -5 && emerge --oneshot portage
-    #unmask_package 'dev-libs/openssl'
-    #update_keywords 'dev-libs/openssl' '**'
+    rm -Rf /var/sync/portage && emerge --sync && etc-update --automode -5 && emerge --oneshot portage
+    sed -i '/dev-libs\/openssl.*/d' /etc/portage/profile/package.provided
+    unmask_package 'dev-libs/openssl'
+    update_keywords 'dev-libs/openssl' '+**'
     update_keywords 'net-proxy/haproxy' '+~amd64'
     update_use 'net-proxy/haproxy' '+crypt +net_ns +pcre +slz +ssl +threads +vim-syntax -zlib'
 }
