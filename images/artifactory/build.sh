fa306a95b77a2fd9cee3fb253297a457e72cd582
Simon Alman <haven@thehavennet.org.uk>
2019-07-03 16:52:22 +0100

    Artifactory 6.11.1 build

--- images/artifactory/build.sh
+++ images/artifactory/build.sh
@@ -1,4 +1,4 @@
-_packages="app-shells/bash =dev-util/artifactory-pro-bin-6.2.0"
+_packages="app-shells/bash =dev-util/artifactory-pro-bin-6.11.1"
 configure_bob()
 {
     :
@@ -6,10 +6,11 @@ configure_bob()
 
 configure_rootfs_build()
 {
+    sed -i '/app-shells\/bash.*/d' /etc/portage/profile/package.provided
     emerge net-misc/curl net-misc/rsync app-portage/layman
     configure_layman
     add_overlay haven-overlay https://github.com/thehaven/haven-overlay.git
-    update_keywords '=dev-util/artifactory-pro-bin-6.2.0' '+~amd64'
+    update_keywords '=dev-util/artifactory-pro-bin-6.11.1' '+~amd64'
     :
 }
 
