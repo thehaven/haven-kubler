517180f65c13b201b3c423d1b135c5434ac8f79c
Simon Alman <haven@thehavennet.org.uk>
2021-04-24 07:30:27 +0100

    minor fix to artifactory build

--- images/artifactory/build.sh
+++ images/artifactory/build.sh
@@ -11,7 +11,7 @@ configure_rootfs_build()
     emerge net-misc/curl net-misc/rsync app-portage/layman
     configure_layman
     add_overlay haven-overlay  https://gitlab.thehavennet.org.uk/gentoo/haven-overlay.git
-    update_keywords 'dev-util/artifactory-pro-bin' '+~amd64'
+    update_keywords 'dev-util/artifactory-pro-bin' '+**'
     groupadd -g 1000 artifactory
     useradd -u 1000 -g artifactory -d /opt/artifactory artifactory
     :
