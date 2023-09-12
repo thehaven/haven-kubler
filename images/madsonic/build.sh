77b261e3fba0e25da0eba8fd888a3c1b8c24df00
Simon Alman <haven@thehavennet.org.uk>
2023-09-12 13:02:36 +0100

    Madsonic final build

--- images/madsonic/build.sh
+++ images/madsonic/build.sh
@@ -21,5 +21,22 @@ configure_rootfs_build()
 
 finish_rootfs_build()
 {
-    mkdir -p /config /media/{default,uploaded,podcast,playlist/{import,export,backup}}
+    CHROOT='/emerge-root'
+    mkdir -p ${CHROOT}/config ${CHROOT}/media/{default,uploaded,podcast,playlist/{import,export,backup}}
+    
+    # Update uid and gid to 1000:1000 within the chroot:
+    if chroot ${CHROOT} id madsonic > /dev/null 2>&1; then 
+        usermod -R ${CHROOT} -u 1000 madsonic
+
+        if chroot ${CHROOT} getent group madsonic > /dev/null 2>&1; then 
+	    groupmod -R ${CHROOT} -g 1000 madsonic
+            usermod -R ${CHROOT} -g 1000 madsonic
+	fi
+    fi
+
+    # Fix up permissions to match the new uid/gid set above:
+    [[ -d ${CHROOT}/usr/libexec/madsonic ]] && chown -Rf 1000:1000 ${CHROOT}/usr/libexec/madsonic
+    [[ -d ${CHROOT}/var/lib/madsonic ]] && chown -Rf 1000:1000 ${CHROOT}/var/lib/madsonic
+    [[ -d ${CHROOT}/config ]] && chown -Rf 1000:1000 ${CHROOT}/config
+
 }
